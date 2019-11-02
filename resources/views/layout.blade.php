c17fda8ad4c2a501f1bde1dbaab1e08617b690b4
piotr-wrobel <piotr.wrobel.stanislaw@gmail.com>
2019-11-02 22:05:58 +0100

    autoryzacja (1)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -72,7 +72,7 @@
         </div>
 
         <div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
+            <a href="pagess">Pagess</a>
             <a href="https://laracasts.com">Laracasts</a>
             <a href="https://laravel-news.com">News</a>
             <a href="https://forge.laravel.com">Forge</a>
